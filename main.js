b5fa20e0cb9e5c9d7d23605f64961e9a144dfc0b
Franco Benitez <Franquitowe02@gmail.com>
2023-03-31 21:05:51 -0300

    cambio 1

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const btn = document.querySelector(".Dropdown")
 const mobileNav = document.querySelector(".nav__container")
 
 btn.addEventListener("click", () => {
-    mobileNav.classList.toggle("mobile__nav")
+    mobileNav.classList.toggle("mobile__nav");
+    btn.classList.toggle("Dropdown__open")
 })
