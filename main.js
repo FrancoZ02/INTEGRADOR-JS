178776fa1bdeae85ea5d2a8a046ed36add96e6ae
Franco Benitez <Franquitowe02@gmail.com>
2023-04-01 18:34:49 -0300

    problema con los nav links

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const btn = document.querySelector(".Dropdown")
 const mobileNav = document.querySelector(".nav__container")
+const line1__bars = document.querySelector(".line1__bars-menu");
+const line2__bars = document.querySelector(".line2__bars-menu");
+const line3__bars = document.querySelector(".line3__bars-menu");
+const navLinks = document.querySelector(".nav__links");
 
 btn.addEventListener("click", () => {
     mobileNav.classList.toggle("mobile__nav");
-    btn.classList.toggle("Dropdown__open")
+    btn.classList.toggle("Dropdown__open");
 })
+
+navLinks.addEventListener("click", () => {
+    mobileNav.classList.toggle("mobile__nav");
+    btn.classList.toggle("Dropdown__open");
+    animateBars();
+})
+
+btn.addEventListener("click", animateBars)
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
