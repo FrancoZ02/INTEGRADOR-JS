3a1a5cb5f500ae6aee4a35ba7be41cff078977ff
Franco Benitez <Franquitowe02@gmail.com>
2023-04-04 17:54:05 -0300

    galeria

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ const line3__bars = document.querySelector(".line3__bars-menu");
 const navLinks = document.querySelector(".items__nav");
 const DOM = document;
 
+// ABRIR O CERRAR MENU DESPLEGABLE EN MOBILE
 const OpensMenu = () => {
     mobileNav.classList.toggle("mobile__nav");
     btn.classList.toggle("Dropdown__open");
     animateBars();
 }
 
+// CERRAR MENU DESPLEGABLE AL HACER SCROLL
 const ScrollDom = () => {
     if (mobileNav.classList.contains("mobile__nav")) {
         mobileNav.classList.toggle("mobile__nav");
@@ -22,12 +24,14 @@ const ScrollDom = () => {
     }
 }
 
+// ANIMACION DEL BOTON DEL MENU DESPLEGABLE
 function animateBars(){
     line1__bars.classList.toggle("activeline1__bars-menu");
     line2__bars.classList.toggle("activeline2__bars-menu");
     line3__bars.classList.toggle("activeline3__bars-menu");
 }
 
+//CERRAR MENU DESPLEGABLE CLICKEANDO EN LOS LINKS
 const CloseOnClick = (e) => {
     if (e.target.classList.contains('.nav__links')) {
         return;
