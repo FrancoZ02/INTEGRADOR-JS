184700be0153e8b17b56dd726d5a8036244d6ef3
Franco Benitez <Franquitowe02@gmail.com>
2023-04-04 16:38:30 -0300

    header resuelto

--- main.js
+++ main.js
@@ -3,23 +3,32 @@ const mobileNav = document.querySelector(".nav__container")
 const line1__bars = document.querySelector(".line1__bars-menu");
 const line2__bars = document.querySelector(".line2__bars-menu");
 const line3__bars = document.querySelector(".line3__bars-menu");
-const navLinks = document.querySelector(".nav__links");
+const navLinks = document.querySelector(".items__nav");
 
-btn.addEventListener("click", () => {
-    mobileNav.classList.toggle("mobile__nav");
-    btn.classList.toggle("Dropdown__open");
-})
-
-navLinks.addEventListener("click", () => {
+const OpensMenu = () => {
     mobileNav.classList.toggle("mobile__nav");
     btn.classList.toggle("Dropdown__open");
     animateBars();
-})
-
-btn.addEventListener("click", animateBars)
+}
 
 function animateBars(){
     line1__bars.classList.toggle("activeline1__bars-menu");
     line2__bars.classList.toggle("activeline2__bars-menu");
     line3__bars.classList.toggle("activeline3__bars-menu");
 }
+
+const CloseOnClick = (e) => {
+    if (e.target.classList.contains('.nav__links')) {
+        return;
+    }
+    mobileNav.classList.remove("mobile__nav");
+    btn.classList.remove("Dropdown__open");
+    animateBars();
+}
+
+const init = () => {
+    btn.addEventListener('click', OpensMenu);
+    navLinks.addEventListener('click', CloseOnClick);
+}
+
+init()
