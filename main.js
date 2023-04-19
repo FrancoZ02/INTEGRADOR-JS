da705f11143ef3a6bf6967ee2bdd6150c4f07d7b
Franco Benitez <Franquitowe02@gmail.com>
2023-04-19 20:12:41 -0300

    overlay

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { shoes } from "./secondary.js";
 const btn = document.querySelector(".dropdown");
 const mobileNav = document.querySelector(".nav__container");
+const overlay = document.querySelector(".overlay");
 const cart = document.querySelector(".cart");
 const cartContainer = document.querySelector(".cart__container");
 const btnCloseCart = document.querySelector('.close__cart')
@@ -12,6 +13,7 @@ const listElements = document.querySelectorAll(".list__button--click");
 // ABRIR O CERRAR MENU DESPLEGABLE EN MOBILE
 const OpensMenu = () => {
     mobileNav.classList.add("visible__nav");
+    overlay.style.display = 'block';
     if (cartContainer.classList.contains('visible__cart')) {
         cartContainer.classList.remove("visible__cart");
     }
@@ -44,8 +46,15 @@ listElements.forEach(listElement => {
     })
 });
 
+const closeOverlay = () => {
+    overlay.style.display= 'none';
+    CloseMenu();
+    CloseCart();
+}
+
 const OpenCart = () => {
     cartContainer.classList.add('visible__cart');
+    overlay.style.display = 'block';
     if (mobileNav.classList.contains('visible__nav')) {
         mobileNav.classList.remove("visible__nav");
     }
@@ -58,6 +67,7 @@ const CloseCart = () => {
 
 const init = () => {
     btn.addEventListener('click', OpensMenu);
+    overlay.addEventListener('click', closeOverlay)
     cart.addEventListener('click', OpenCart);
     btnCloseCart.addEventListener('click', CloseCart);
     CloseMenuBtn.addEventListener('click', CloseMenu);
