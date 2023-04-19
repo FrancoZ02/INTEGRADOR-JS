57364b6cee8bc7ed51dd2927812b3bc0de0e2315
Franco Benitez <Franquitowe02@gmail.com>
2023-04-19 19:39:37 -0300

    cerrar y abrir cart menu

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import { shoes } from "./secondary.js";
 const btn = document.querySelector(".dropdown");
 const mobileNav = document.querySelector(".nav__container");
-const navLinks = document.querySelector(".items__nav");
-const CloseMenuBtn = document.querySelector(".close__menu")
+const cart = document.querySelector(".cart");
+const cartContainer = document.querySelector(".cart__container");
+const btnCloseCart = document.querySelector('.close__cart')
+const navLinks = document.querySelector(".list__item");
+const CloseMenuBtn = document.querySelector(".close__menu");
 const DOM = document;
 const listElements = document.querySelectorAll(".list__button--click");
 
 // ABRIR O CERRAR MENU DESPLEGABLE EN MOBILE
 const OpensMenu = () => {
     mobileNav.classList.add("visible__nav");
+    if (cartContainer.classList.contains('visible__cart')) {
+        cartContainer.classList.remove("visible__cart");
+    }
+    return
 }
 
 const CloseMenu = () => {
@@ -17,7 +24,7 @@ const CloseMenu = () => {
 
 //CERRAR MENU DESPLEGABLE CLICKEANDO EN LOS LINKS
 const CloseOnClick = (e) => {
-    if (e.target.classList.contains('.nav__links')) {
+    if (e.target.classList.contains('.nav__link')) {
         return;
     }
     mobileNav.classList.remove("visible__nav");
@@ -37,13 +44,24 @@ listElements.forEach(listElement => {
     })
 });
 
+const OpenCart = () => {
+    cartContainer.classList.add('visible__cart');
+    if (mobileNav.classList.contains('visible__nav')) {
+        mobileNav.classList.remove("visible__nav");
+    }
+    return
+}
+
+const CloseCart = () => {
+    cartContainer.classList.remove('visible__cart');
+}
+
 const init = () => {
     btn.addEventListener('click', OpensMenu);
+    cart.addEventListener('click', OpenCart);
+    btnCloseCart.addEventListener('click', CloseCart);
     CloseMenuBtn.addEventListener('click', CloseMenu);
     navLinks.addEventListener('click', CloseOnClick);
-    DOM.addEventListener('scroll', ScrollDom);
 }
 
 init()
-
-hola()
